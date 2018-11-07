ba010aed948ba1af72e75d6bd72fb2c082abf3ab
Julien Blanchet <j55blanchet@gmail.com>
2018-11-07 14:51:19 -0500

    Adding sound

--- docs/treasure-hunt-class/boy.js
+++ docs/treasure-hunt-class/boy.js
@@ -4,7 +4,10 @@ class Boy {
         this.col = col;
         this.row = row;
         this.img =loadImage('assets/boy/idle/frame-1.png');
+        this.moveSound = loadSound('assets/boy/footsteps.wav');
         this.direction = DIRECTION.RIGHT;
+
+        this.moveSound.setVolume(0.4);
     }
 
     draw() {
@@ -15,24 +18,28 @@ class Boy {
         if (this.col < grid.cols - 1) {
             this.col += 1
             this.direction = DIRECTION.RIGHT
+            this.moveSound.play()
         }
     }
     moveLeft() {
         if (this.col > 0) {
             this.col -= 1
             this.direction = DIRECTION.LEFT
+            this.moveSound.play()
         }
     }
     moveUp() {
         if (this.row > 0) {
             this.row -= 1
             this.direction = DIRECTION.UP
+            this.moveSound.play();
         }
     }
     moveDown() {
         if (this.row < grid.rows - 1) {
             this.row += 1
             this.direction = DIRECTION.DOWN
+            this.moveSound.play();
         }
     }
 }
