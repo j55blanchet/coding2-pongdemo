4d98f2fb294518f3ca4afb77271233b682109fb4
Julien Blanchet <j55blanchet@gmail.com>
2019-03-06 11:25:38 -0500

    Class updates

--- docs/paintshop/paintshop.js
+++ docs/paintshop/paintshop.js
@@ -4,14 +4,25 @@
 
 
 
-const redColor = color(238, 0, 0);
-
+let redColor
+let orangeColor;
+let blueColor;
+let purpleColor;
+let greenColor;
+let whiteColor;
 
 function preload() {
+    redColor = color(238, 0, 0);
+    orangeColor = color(238, 108, 0);
+    blueColor = color(0, 143, 143);
+    purpleColor = color(115, 0, 172);
+    greenColor = color(0, 190, 0);
+    whiteColor = color(255);
 }
 
+let canvasObject;
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    canvasObject = createCanvas(windowWidth, windowHeight);
     background(255);
 }
 
@@ -20,6 +31,40 @@ function draw() {
 
 function mouseDragged() {
     line(pmouseX, pmouseY, mouseX, mouseY);
+}
+
+
+function setBrush(color) {
+    if (color == 'red'){
+        stroke(redColor);
+    }
+    else if (color == 'orange') {
+        stroke(orangeColor);
+    } 
+    else if (color == 'blue') {
+        stroke(blueColor);
+    }
+    else if (color == 'purple') {
+        stroke(purpleColor);
+    }
+    else if (color == 'green') {
+        stroke(greenColor);
+    }
+    else if (color == 'white') {
+        stroke(whiteColor);
+    }
+    else {
+        throw "Invalid color specified: " + color;
+    }
+}
+
+function setCustomColor(colorString) {
+    let c  = color(colorString);
+    stroke(c);
+}
+
+function save() {
+    saveCanvas(canvasObject, 'myImage', 'png');
 }
 
 // // disabled for now because we don't
